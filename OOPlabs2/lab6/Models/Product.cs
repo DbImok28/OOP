7c9f63bc2fc527fa0484ad103566a38de776518f
RubyCircle <pavel.jakushik@yandex.by>
2022-04-11 13:23:02 +0300

    Add description and country

--- OOPlabs2/lab6/Models/Product.cs
+++ OOPlabs2/lab6/Models/Product.cs
@@ -9,12 +9,14 @@ namespace lab6.Models
     [Serializable]
     public class Product
     {
-        public Product(string name, string fullName, int price, string image)
+        public Product(string name, string fullName, int price, string image, string description, string country)
         {
             Name = name;
             FullName = fullName;
             Price = price;
             Image = image;
+            Description = description;
+            Country = country;
         }
         public Product()
         {
@@ -22,10 +24,14 @@ namespace lab6.Models
             FullName = "None";
             Price = -1;
             Image = "None";
+            Description = "None";
+            Country = "None";
         }
 
         public string Name { get; set; }
         public string FullName { get; set; }
+        public string Description { get; set; }
+        public string Country { get; set; }
         public decimal Price { get; set; }
         public string Image { get; set; }
     }
